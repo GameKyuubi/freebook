c05e00e8eef0913993cb9dd295888d7fee0b34a7
GameKyuubi <xtra.zero@gmail.com>
2017-09-21 20:24:16 +0900

    Test node-isbn on client side

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,18 @@
+var isbn = require('node-isbn');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  console.log('HELLO');
+  isbn.resolve('9784088728988', function (err, book) {
+    if (err) {
+      console.log('Book not found', err);
+    } else {
+      console.log('Book found %j', book);
+    }
+    res.send(book);
+  });
 });
 
 module.exports = router;
